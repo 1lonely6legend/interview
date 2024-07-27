89f3f8b7a656f30de44a2b579f269d9ab0aacf01
ogier_windows <haomiao_li@outlook.com>
2024-07-28 03:24:58 +0800

    修改a*和dijkstra结构。改了a*之前的bug

--- search/a_star.cpp
+++ search/a_star.cpp
@@ -3,15 +3,24 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-
-class Node {
- public:
-  Node(int x, int y, int index, int cost, int f, Node* pre)
-      : x(x), y(y), index(index), cost(cost), f(f), pre(pre){};
-  int x, y, index, cost, f;
-  Node* pre;
+struct Point {
+  size_t x, y;
+  Point(size_t x, size_t y): x(x), y(y) {
+  };
 };
-string get_path(Node* end) {
+class Node {
+  public:
+    Node(int x, int y, int index, int cost, int f, Node *pre)
+      : x(x), y(y), index(index), cost(cost), f(f), pre(pre) {
+    };
+    //明确，a*相比较于dijkstra，在pq中需要比较的是f值，也就是cost+H值
+    int x, y, index, cost, f;
+    Node *pre;
+};
+struct cmp {
+  bool operator()(Node *const a, Node *const b) { return a->f > b->f; }
+};
+string get_path(Node *end) {
   string res;
   while (end != nullptr) {
     int x = end->x;
@@ -23,54 +32,66 @@ string get_path(Node* end) {
   reverse(res.begin(), res.end());
   return res;
 };
-struct cmp {
-  bool operator()(Node* const a, Node* const b) { return a->f > b->f; }
-};
-string a_star(const vector<vector<int>>& grid) {
-  const int row = grid.size();
-  const int col = grid[0].size();
-  auto H = [row, col](int x, int y) {
-    return row - x + col - y;
+
+string a_star(const vector<vector<int> > &grid, const Point &start_point, const Point &end_point) {
+  const size_t row = grid.size();
+  const size_t col = grid[0].size();
+  //使用匿名函数,输入当前位置，计算与目标点启发式距离，这里使用曼哈顿距离
+  auto H = [&](size_t x, size_t y) {
+    return (x >= end_point.x)
+             ? (x - end_point.x)
+             : (end_point.x - x)
+               + (y >= end_point.y)
+                 ? (y - end_point.y)
+                 : (end_point.y - y);
   };
-  vector<vector<int>> cost_map(row, vector<int>(col, INT_MAX));
-  priority_queue<Node*, vector<Node*>, cmp> pq;
+  //之前的使用二维数组存放cost的方法，也可以，但我还是更喜欢用index+hash
+  //  vector<vector<int> > cost_map(row, vector<int>(col, INT_MAX));
+  unordered_map<int, int> cost_map;
+  priority_queue<Node *, vector<Node *>, cmp> pq;
   int moves[4][2] = {1, 0, 0, 1, -1, 0, 0, -1};
 
-  Node* start = new Node(0, 0, 0, 0, H(0, 0), nullptr);
+  Node *start = new Node(0, 0, 0, 0, 0 + H(0, 0), nullptr);
   pq.push(start);
-  cost_map[0][0] = 0;
+  cost_map[0] = 0;
 
   while (!pq.empty()) {
-    Node* cur = pq.top();
+    Node *cur = pq.top();
     pq.pop();
-    if (cur->x == row - 1 && cur->y == col - 1) {
+    if (cur->x == end_point.x && cur->y == end_point.y) {
       return get_path(cur);
     }
-    if (cost_map[cur->x][cur->y] < cur->cost) {
-      // Avoid processing if a better path is found
+    //如果当前要处理的节点，已经有更小的cost了，那么就不用再处理了
+    if (cost_map[cur->index] < cur->cost) {
       continue;
     }
-    for (auto move : moves) {
-      int next_x = cur->x + move[0];
-      int next_y = cur->y + move[1];
+    for (const auto &[dx,dy] : moves) {
+      int next_x = cur->x + dx;
+      int next_y = cur->y + dy;
       if (next_x >= 0 && next_y >= 0 && next_x < row && next_y < col) {
+        int index = next_x * col + next_y;
         int next_cost = cur->cost + grid[next_x][next_y];
+        //这里是a*的关键，f = g + h
         int f = next_cost + H(next_x, next_y);
-        if (next_cost < cost_map[next_x][next_y]) {
-          cost_map[next_x][next_y] = next_cost;
-          pq.push(new Node(next_x, next_y, next_x * col + next_y, next_cost, f,
-                           cur));
+        //如果这个点没有被访问过，或者被访问过，但是这次的代价更小，那么就更新这个点的代价，并且加入pq
+        if (cost_map.find(index) == cost_map.end() || cost_map[index] > next_cost) {
+          pq.push(new Node(next_x, next_y, index, next_cost, f, cur));
+          cost_map[index] = next_cost;
         }
       }
     }
   }
-  return "Path not found";
+  return "path not found";
 }
 
 int main() {
-  vector<vector<int>> grid = {
-      {1, 9, 1, 9, 1}, {1, 5, 1, 1, 1}, {1, 2, 2, 2, 4}, {1, 2, 3, 4, 5}};
+  vector<vector<int> > grid = {
+    {1, 9, 1, 9, 1},
+    {1, 5, 1, 1, 1},
+    {1, 2, 2, 2, 4},
+    {1, 2, 3, 4, 5}
+  };
 
-  cout << "path:" << a_star(grid) << endl;
+  cout << "path:" << a_star(grid, {0, 0}, {grid.size() - 1, grid[0].size() - 1}) << endl;
   return 0;
 }
--- search/dijkstra.cpp
+++ search/dijkstra.cpp
@@ -3,15 +3,27 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-
-class Node {
- public:
-  Node(int x, int y, int index, int cost, Node* pre)
-      : x(x), y(y), index(index), cost(cost), pre(pre){};
-  int x, y, index, cost;
-  Node* pre;
+struct Point {
+  size_t x, y;
+  Point(size_t x, size_t y): x(x), y(y) {
+  };
 };
-string get_path(Node* end) {
+//定义节点类
+class Node {
+  public:
+    Node(int x, int y, int index, int cost, Node *pre)
+      : x(x), y(y), index(index), cost(cost), pre(pre) {
+    };
+    int x, y, index, cost;
+    Node *pre;
+};
+//提供给pq的比较函数
+struct cmp {
+  //注意，搞清楚这部分的逻辑，这里要使用的是小顶堆
+  bool operator()(Node *const a, Node *const b) { return a->cost > b->cost; }
+};
+//找到终点之后，根据pre指针，逆序找到路径，并打印
+string get_path(Node *end) {
   string res;
   while (end != nullptr) {
     int x = end->x;
@@ -23,52 +35,58 @@ string get_path(Node* end) {
   reverse(res.begin(), res.end());
   return res;
 };
-struct cmp {
-  bool operator()(Node* const a, Node* const b) { return a->cost > b->cost; }
-};
-string dijkstra(const vector<vector<int>>& grid) {
-  const int row = grid.size();
-  const int col = grid[0].size();
+
+string dijkstra(const vector<vector<int> > &grid,
+                const Point &start_point,
+                const Point &end_point) {
+  const size_t row = grid.size();
+  const size_t col = grid[0].size();
+  //使用一个哈希表，记录当前遍历过程中，遇到的点的**最小**代价，相当于也记录了哪些点没有被访问过
   unordered_map<int, int> cost_map;
-  priority_queue<Node*, vector<Node*>, cmp> pq;
+  //使用pq来进行优先级队列的操作，每次取出代价最小的点来进行查找
+  priority_queue<Node *, vector<Node *>, cmp> pq;
   int moves[4][2] = {1, 0, 0, 1, -1, 0, 0, -1};
 
-  Node* start = new Node(0, 0, 0, 0, nullptr);
+  Node *start = new Node(start_point.x, start_point.y, 0, 0, nullptr);
   pq.push(start);
   cost_map[0] = 0;
 
   while (!pq.empty()) {
-    Node* cur = pq.top();
+    Node *cur = pq.top();
     pq.pop();
-    if (cur->x == row - 1 && cur->y == col - 1) {
+    if (cur->x == end_point.x && cur->y == end_point.y) {
       return get_path(cur);
     }
     if (cost_map[cur->index] < cur->cost) {
-      //如果遇到这个点之前已经被访问过了，并且更新了更小的cost，但是没有更新压入pq中的cost
-      //这里需要判断，如果之前的cost比当前的cost小，就不需要再次访问这个点
+      //代表这个点之前已被访问，并且之前访问此位置的代价更小，但是这次弹出的pq中的cost是代价更大的一个路径，因此不对这个点进行处理
       continue;
     }
-    for (auto move : moves) {
-      int next_x = cur->x + move[0];
-      int next_y = cur->y + move[1];
+    for (const auto &[dx,dy] : moves) {
+      int next_x = cur->x + dx;
+      int next_y = cur->y + dy;
       if (next_x >= 0 && next_y >= 0 && next_x < row && next_y < col) {
         int index = next_x * col + next_y;
         int cost = cur->cost + grid[next_x][next_y];
+        // 下面两种情况需要更新代价，并压入pq。 1.如果当前发现了还没有被访问过的点，直接加入pq  2.或者如果发现了比之前代价更小的路径
+        // 注意要先判断是否存在，然后再判断代价，防止越界
         if (cost_map.find(index) == cost_map.end() || cost_map[index] > cost) {
-          Node* next = new Node(next_x, next_y, index, cost, cur);
-          pq.push(next);
+          pq.push(new Node(next_x, next_y, index, cost, cur));
           cost_map[index] = cost;
         }
       }
     }
   }
-  return "tmd not find";
+  return "path not found";
 }
 
 int main() {
-  vector<vector<int>> grid = {
-      {1, 9, 1, 9, 1}, {1, 5, 1, 1, 1}, {1, 2, 2, 2, 4}, {1, 2, 3, 4, 5}};
+  vector<vector<int> > grid = {
+    {1, 9, 1, 9, 1},
+    {1, 5, 1, 1, 1},
+    {1, 2, 2, 2, 4},
+    {1, 2, 3, 4, 5}
+  };
 
-  cout << "path:" << dijkstra(grid) << endl;
+  cout << "path:" << dijkstra(grid, {0, 0}, {grid.size() - 1, grid[0].size() - 1}) << endl;
   return 0;
 }
